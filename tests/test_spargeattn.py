d428b452bdbd6174064b9335c077c7aa7c82ff47
woctordho <woctordho@outlook.com>
2025-07-29 18:15:45 +0800

    Add test_spargeattn.py

---
+++ tests/test_spargeattn.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import torch
+import torch.nn.functional as F
+from spas_sage_attn import spas_sage2_attn_meansim_cuda
+from torch.nn.attention import SDPBackend, sdpa_kernel
+
+
+def get_rtol_atol(actual, expect):
+    actual = actual.float()
+    expect = expect.float()
+    diff = (actual - expect).abs()
+    eps = torch.tensor(
+        torch.finfo(actual.dtype).eps, device=actual.device, dtype=actual.dtype
+    )
+    rdiff = diff / torch.maximum(torch.maximum(actual.abs(), expect.abs()), eps)
+    return (
+        f"mean_rtol={rdiff.mean().item():.3g} "
+        f"max_rtol={rdiff.max().item():.3g} "
+        f"mean_atol={diff.max().item():.3g} "
+        f"max_atol={diff.max().item():.3g}"
+    )
+
+
+def main():
+    batch_size = 4
+    head_num = 32
+    seq_len = 128
+    head_dim = 128
+    dtype = torch.float16
+
+    q = torch.randn(batch_size, head_num, seq_len, head_dim, device="cuda", dtype=dtype)
+    k = torch.randn_like(q)
+    v = torch.randn_like(q)
+    print("q", tuple(q.shape), q.device, q.dtype)
+
+    # 'Mathematically correct' implementation
+    torch.backends.cuda.enable_math_sdp(True)
+    with sdpa_kernel(SDPBackend.MATH):
+        out_math = F.scaled_dot_product_attention(q, k, v)
+
+    out_sparge = spas_sage2_attn_meansim_cuda(q, k, v)
+    print("sparge vs math:", get_rtol_atol(out_sparge, out_math))
+    print("The above (except max_rtol) should be < 0.05 (on RTX 20xx/30xx) or < 0.1 (on RTX 40xx/50xx)")
+
+
+if __name__ == "__main__":
+    main()
