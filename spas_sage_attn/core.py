005135c7852a2f0fdfb4705bd410a2f9d8fe8649
woctordho <woctordho@outlook.com>
2025-07-29 18:23:36 +0800

    Use the same API for sm80 and sm89/90

--- spas_sage_attn/core.py
+++ spas_sage_attn/core.py
@@ -22,19 +22,13 @@ from einops import rearrange
 import spas_sage_attn._qattn as qattn
 import spas_sage_attn._fused as fused
 
-SAGE2PP_ENABLED = True
-try:
-    from spas_sage_attn._qattn import qk_int8_sv_f8_accum_f16_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold
-except:
-    print("Warning: Sage2++ NOT enabled")
-    SAGE2PP_ENABLED = False
-
 def get_cuda_arch_versions():
     cuda_archs = []
     for i in range(torch.cuda.device_count()):
         major, minor = torch.cuda.get_device_capability(i)
         cuda_archs.append(f"sm{major}{minor}")
     return cuda_archs
+
 
 @torch.compiler.disable
 def spas_sage2_attn_meansim_cuda(q, k, v, attn_mask=None, dropout_p=0.0, is_causal=False, scale=None, smooth_k=True, simthreshd1=0.6, cdfthreshd=0.98, pvthreshd=50, attention_sink=False, tensor_layout="HND", output_dtype=torch.float16, return_sparsity=False):
@@ -67,13 +61,8 @@ def spas_sage2_attn_meansim_cuda(q, k, v, attn_mask=None, dropout_p=0.0, is_caus
     assert headdim in [64, 128], "headdim should be in [64, 128]. For other headdim, you can use padding and specify the softmax scale."
 
     pvthreshd = hyperparameter_check(pvthreshd, q.size(-3), q.device)
-    o = torch.empty_like(q)
 
-    if arch in ("sm80", "sm86", "sm87"):
-        qattn.qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold(
-            q_int8, k_int8, v, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, 1, False, 1, scale, 0
-        )
-    else:
+    if arch in {"sm89", "sm90", "sm120"}:
         ## quant v
         b, h_kv, kv_len, head_dim = v.shape
         padded_len = (kv_len + 127) // 128 * 128
@@ -81,18 +70,28 @@ def spas_sage2_attn_meansim_cuda(q, k, v, attn_mask=None, dropout_p=0.0, is_caus
         fused.transpose_pad_permute_cuda(v, v_transposed_permutted, 1)
         v_fp8 = torch.empty(v_transposed_permutted.shape, dtype=torch.float8_e4m3fn, device=v.device)
         v_scale = torch.empty((b, h_kv, head_dim), dtype=torch.float32, device=v.device)
-        #fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 448.0, 1)
-        fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 2.25, 1)
-       
-        if arch == "sm90":
-            qattn.qk_int8_sv_f8_accum_f32_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold_sm90(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, False, 1, scale, 0)
-        elif SAGE2PP_ENABLED:
-            qk_int8_sv_f8_accum_f16_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, False, 1, scale, 0)
+        if arch in {"sm89", "sm120"}:
+            # accum_f16
+            fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 2.25, 1)
         else:
-            qattn.qk_int8_sv_f8_accum_f32_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, False, 1, scale, 0)
-    
+            # accum_f32
+            fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 448.0, 1)
+
+    _is_causal = 1 if is_causal else 0
+    o = torch.empty_like(q)
+
+    if arch in {"sm80", "sm86", "sm87"}:
+        qattn.qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold(q_int8, k_int8, v, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, 1, _is_causal, 1, scale, 0)
+    elif arch in {"sm89", "sm120"}:
+        qattn.qk_int8_sv_f8_accum_f16_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, _is_causal, 1, scale, 0)
+    elif arch == "sm90":
+        qattn.qk_int8_sv_f8_accum_f32_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold_sm90(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, _is_causal, 1, scale, 0)
+    else:
+        raise ValueError(f"Unsupported CUDA architecture: {arch}")
+
     if tensor_layout == 'NHD':
         o = rearrange(o, '... H L D -> ... L H D')
+
     if return_sparsity:
         if is_causal is False:
             qk_sparsity = 1 - (valid_block_num.float().sum()) / (lut.size(3) * lut.size(2) * lut.size(0) * lut.size(1))
@@ -101,6 +100,7 @@ def spas_sage2_attn_meansim_cuda(q, k, v, attn_mask=None, dropout_p=0.0, is_caus
         return o, qk_sparsity.item()
     else:
         return o
+
 
 @torch.compiler.disable
 def spas_sage2_attn_meansim_topk_cuda(q, k, v, attn_mask=None, dropout_p=0.0, is_causal=False, scale=None, smooth_k=True, simthreshd1=-0.1, cdfthreshd=None, topk=0.5, pvthreshd=50, attention_sink=False, tensor_layout="HND", output_dtype=torch.float16, return_sparsity=False):
@@ -133,13 +133,8 @@ def spas_sage2_attn_meansim_topk_cuda(q, k, v, attn_mask=None, dropout_p=0.0, is
     assert headdim in [64, 128], "headdim should be in [64, 128]. For other headdim, you can use padding and specify the softmax scale."
 
     pvthreshd = hyperparameter_check(pvthreshd, q.size(-3), q.device)
-    o = torch.empty_like(q)
 
-    if arch in ("sm80", "sm86", "sm87"):
-        qattn.qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold(
-            q_int8, k_int8, v, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, 1, False, 1, scale, 0
-        )
-    else:
+    if arch in {"sm89", "sm90", "sm120"}:
         ## quant v
         b, h_kv, kv_len, head_dim = v.shape
         padded_len = (kv_len + 127) // 128 * 128
@@ -147,18 +142,28 @@ def spas_sage2_attn_meansim_topk_cuda(q, k, v, attn_mask=None, dropout_p=0.0, is
         fused.transpose_pad_permute_cuda(v, v_transposed_permutted, 1)
         v_fp8 = torch.empty(v_transposed_permutted.shape, dtype=torch.float8_e4m3fn, device=v.device)
         v_scale = torch.empty((b, h_kv, head_dim), dtype=torch.float32, device=v.device)
-        #fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 448.0, 1)
-        fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 2.25, 1)
-        
-        if arch == "sm90":
-            qattn.qk_int8_sv_f8_accum_f32_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold_sm90(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, False, 1, scale, 0)
-        elif SAGE2PP_ENABLED:
-            qk_int8_sv_f8_accum_f16_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, False, 1, scale, 0)
+        if arch in {"sm89", "sm120"}:
+            # accum_f16
+            fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 2.25, 1)
         else:
-            qattn.qk_int8_sv_f8_accum_f32_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, False, 1, scale, 0)
-    
+            # accum_f32
+            fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 448.0, 1)
+
+    _is_causal = 1 if is_causal else 0
+    o = torch.empty_like(q)
+
+    if arch in {"sm80", "sm86", "sm87"}:
+        qattn.qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold(q_int8, k_int8, v, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, 1, _is_causal, 1, scale, 0)
+    elif arch in {"sm89", "sm120"}:
+        qattn.qk_int8_sv_f8_accum_f16_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, _is_causal, 1, scale, 0)
+    elif arch == "sm90":
+        qattn.qk_int8_sv_f8_accum_f32_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold_sm90(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, _is_causal, 1, scale, 0)
+    else:
+        raise ValueError(f"Unsupported CUDA architecture: {arch}")
+
     if tensor_layout == 'NHD':
         o = rearrange(o, '... H L D -> ... L H D')
+
     if return_sparsity:
         if is_causal is False:
             qk_sparsity = 1 - (valid_block_num.float().sum()) / (lut.size(3) * lut.size(2) * lut.size(0) * lut.size(1))
@@ -167,7 +172,8 @@ def spas_sage2_attn_meansim_topk_cuda(q, k, v, attn_mask=None, dropout_p=0.0, is
         return o, qk_sparsity.item()
     else:
         return o
-    
+
+
 @torch.compiler.disable
 def block_sparse_sage2_attn_cuda(q, k, v, mask_id=None, dropout_p=0.0, scale=None, smooth_k=True, pvthreshd=50, attention_sink=False, tensor_layout="HND", output_dtype=torch.float16, return_sparsity=False):
     assert tensor_layout in ['HND', 'NHD']
@@ -200,13 +206,8 @@ def block_sparse_sage2_attn_cuda(q, k, v, mask_id=None, dropout_p=0.0, scale=Non
     assert headdim in [64, 128], "headdim should be in [64, 128]. For other headdim, you can use padding and specify the softmax scale."
 
     pvthreshd = hyperparameter_check(pvthreshd, q.size(-3), q.device)
-    o = torch.empty_like(q)
 
-    if arch in ("sm80", "sm86", "sm87"):
-        qattn.qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold(
-            q_int8, k_int8, v, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, 1, False, 1, scale, 0
-        )
-    else:
+    if arch in {"sm89", "sm90", "sm120"}:
         ## quant v
         b, h_kv, kv_len, head_dim = v.shape
         padded_len = (kv_len + 127) // 128 * 128
@@ -214,106 +215,34 @@ def block_sparse_sage2_attn_cuda(q, k, v, mask_id=None, dropout_p=0.0, scale=Non
         fused.transpose_pad_permute_cuda(v, v_transposed_permutted, 1)
         v_fp8 = torch.empty(v_transposed_permutted.shape, dtype=torch.float8_e4m3fn, device=v.device)
         v_scale = torch.empty((b, h_kv, head_dim), dtype=torch.float32, device=v.device)
-        #fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 448.0, 1)
-        fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 2.25, 1)
-        
-        if arch == "sm90":
-            qattn.qk_int8_sv_f8_accum_f32_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold_sm90(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, False, 1, scale, 0)
-        elif SAGE2PP_ENABLED:
-            qk_int8_sv_f8_accum_f16_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, False, 1, scale, 0)
+        if arch in {"sm89", "sm120"}:
+            # accum_f16
+            fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 2.25, 1)
         else:
-            qattn.qk_int8_sv_f8_accum_f32_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, False, 1, scale, 0)
-    
+            # accum_f32
+            fused.scale_fuse_quant_cuda(v_transposed_permutted, v_fp8, v_scale, kv_len, 448.0, 1)
+
+    o = torch.empty_like(q)
+
+    if arch in {"sm80", "sm86", "sm87"}:
+        qattn.qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold(q_int8, k_int8, v, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, 1, False, 1, scale, 0)
+    elif arch in {"sm89", "sm120"}:
+        qattn.qk_int8_sv_f8_accum_f16_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, False, 1, scale, 0)
+    elif arch == "sm90":
+        qattn.qk_int8_sv_f8_accum_f32_block_sparse_attn_inst_buf_fuse_v_scale_with_pv_threshold_sm90(q_int8, k_int8, v_fp8, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, v_scale, 1, False, 1, scale, 0)
+    else:
+        raise ValueError(f"Unsupported CUDA architecture: {arch}")
+
     if tensor_layout == 'NHD':
         o = rearrange(o, '... H L D -> ... L H D')
+
     if return_sparsity:
         qk_sparsity = 1 - (valid_block_num.float().sum()) / ((lut.size(3) + 2) // 2 * lut.size(2) * lut.size(0) * lut.size(1))
         return o, qk_sparsity.item()
     else:
         return o
 
-@torch.compiler.disable
-def spas_sage_attn_meansim_cuda(q, k, v, attn_mask=None, dropout_p=0.0, is_causal=False, scale=None, smooth_k=True, simthreshd1=0.6, cdfthreshd=0.98, pvthreshd=50, attention_sink=False, tensor_layout="HND", output_dtype=torch.float16, return_sparsity=False):
-    assert tensor_layout in ['HND', 'NHD']
-    if tensor_layout == 'NHD':
-        q, k, v = map(lambda t: rearrange(t, '... L H D -> ... H L D'), (q, k, v))
-    assert q.size(-2)>=128, "seq_len should be not less than 128."
-    torch.cuda.set_device(v.device)
 
-    dtype = q.dtype
-    if dtype == torch.float32 or dtype == torch.float16:
-        q, k, v = q.contiguous().to(torch.float16), k.contiguous().to(torch.float16), v.contiguous().to(torch.float16)
-    else:
-        q, k, v = q.contiguous().to(torch.bfloat16), k.contiguous().to(torch.bfloat16), v.contiguous().to(torch.float16)
-
-    if smooth_k:
-        km = k.mean(dim=-2, keepdim=True)
-        # k = k - km
-    headdim = q.size(-1)
-
-    lut, valid_block_num, q_int8, q_scale, k_int8, k_scale = get_block_map_meansim_fuse_quant(q, k, km, is_causal=is_causal, simthreshd1=simthreshd1, cdfthreshd=cdfthreshd, return_lut=True, attention_sink=attention_sink)  # 
-
-    if scale is None:
-        scale = 1.0 / (headdim ** 0.5)
-
-    assert headdim in [64, 128], "headdim should be in [64, 128]. For other headdim, you can use padding and specify the softmax scale."
-
-    pvthreshd = hyperparameter_check(pvthreshd, q.size(-3), q.device)
-
-    _is_causal = 1 if is_causal else 0
-    o = torch.empty_like(q)
-    qattn.qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold(q_int8, k_int8, v, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, 1, _is_causal, 1, scale, 0)
-    if tensor_layout == 'NHD':
-        o = rearrange(o, '... H L D -> ... L H D')
-
-    if return_sparsity:
-        if is_causal is False:
-            qk_sparsity = 1 - (valid_block_num.float().sum()) / (lut.size(3) * lut.size(2) * lut.size(0) * lut.size(1))
-        else:
-            qk_sparsity = 1 - (valid_block_num.float().sum()) / ((lut.size(3) + 2) // 2 * lut.size(2) * lut.size(0) * lut.size(1))
-        return o, qk_sparsity.item()
-    else:
-        return o
-
-@torch.compiler.disable
-def spas_sage_attn_meansim_topk_cuda(q, k, v, attn_mask=None, dropout_p=0.0, is_causal=False, scale=None, smooth_k=True, simthreshd1=-0.1, cdfthreshd=None, topk=0.5, pvthreshd=50, attention_sink=False, tensor_layout="HND", output_dtype=torch.float16, return_sparsity=False):
-    assert tensor_layout in ['HND', 'NHD']
-    if tensor_layout == 'NHD':
-        q, k, v = map(lambda t: rearrange(t, '... L H D -> ... H L D'), (q, k, v))
-    assert q.size(-2)>=128, "seq_len should be not less than 128."
-    torch.cuda.set_device(v.device)
-
-    dtype = q.dtype
-    if dtype == torch.float32 or dtype == torch.float16:
-        q, k, v = q.contiguous().to(torch.float16), k.contiguous().to(torch.float16), v.contiguous().to(torch.float16)
-    else:
-        q, k, v = q.contiguous().to(torch.bfloat16), k.contiguous().to(torch.bfloat16), v.contiguous().to(torch.float16)
-
-    if smooth_k:
-        km = k.mean(dim=-2, keepdim=True)
-        # k = k - km
-    headdim = q.size(-1)
-
-    lut, valid_block_num, q_int8, q_scale, k_int8, k_scale = get_block_map_meansim_fuse_quant(q, k, km, is_causal=is_causal, simthreshd1=simthreshd1, cdfthreshd=cdfthreshd, topk=topk, return_lut=True, attention_sink=attention_sink)  # 
-
-    if scale is None:
-        scale = 1.0 / (headdim ** 0.5)
-
-    assert headdim in [64, 128], "headdim should be in [64, 128]. For other headdim, you can use padding and specify the softmax scale."
-
-    pvthreshd = hyperparameter_check(pvthreshd, q.size(-3), q.device)
-
-    _is_causal = 1 if is_causal else 0
-    o = torch.empty_like(q)
-    qattn.qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold(q_int8, k_int8, v, o, lut, valid_block_num, pvthreshd, q_scale, k_scale, 1, _is_causal, 1, scale, 0)
-    if tensor_layout == 'NHD':
-        o = rearrange(o, '... H L D -> ... L H D')
-
-    if return_sparsity:
-        if is_causal is False:
-            qk_sparsity = 1 - (valid_block_num.float().sum()) / (lut.size(3) * lut.size(2) * lut.size(0) * lut.size(1))
-        else:
-            qk_sparsity = 1 - (valid_block_num.float().sum()) / ((lut.size(3) + 2) // 2 * lut.size(2) * lut.size(0) * lut.size(1))
-        return o, qk_sparsity.item()
-    else:
-        return o
+# For compatibility. spas_sage2_attn_meansim_cuda, spas_sage2_attn_meansim_topk_cuda, block_sparse_sage2_attn_cuda already support sm80/86/89/90/120
+spas_sage_attn_meansim_cuda = spas_sage2_attn_meansim_cuda
+spas_sage_attn_meansim_topk_cuda = spas_sage2_attn_meansim_topk_cuda
