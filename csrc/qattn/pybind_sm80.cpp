7a8b06c0dd9e01bd7895479d5b10de1e65eafb83
woctordho <woctordho@outlook.com>
2025-07-31 11:03:47 +0800

    Port to ABI3

--- csrc/qattn/pybind_sm80.cpp
+++ csrc/qattn/pybind_sm80.cpp
@@ -14,11 +14,66 @@
  * limitations under the License.
  */
 
-#include <pybind11/pybind11.h>
+#include <Python.h>
+#include <torch/library.h>
+
 #include "attn_cuda_sm80.h"
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
-{
-  m.def("qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf", &qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf);
-  m.def("qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold", &qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold);
+extern "C" {
+    /* Creates a dummy empty _C module that can be imported from Python.
+       The import from Python will load the .so consisting of this file
+       in this extension, so that the TORCH_LIBRARY static initializers
+       below are run. */
+    PyObject* PyInit__qattn_sm80(void)
+    {
+        static struct PyModuleDef module_def = {
+            PyModuleDef_HEAD_INIT,
+            "_qattn_sm80",  /* name of module */
+            NULL,           /* module documentation, may be NULL */
+            -1,             /* size of per-interpreter state of the module,
+                               or -1 if the module keeps state in global variables. */
+            NULL,           /* methods */
+        };
+        return PyModule_Create(&module_def);
+    }
+}
+
+// Defines the operators
+TORCH_LIBRARY(spas_sage_attn_qattn_sm80, m) {
+    m.def("qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf("
+            "Tensor query, "
+            "Tensor key, "
+            "Tensor value, "
+            "Tensor(a!) output, "
+            "Tensor lut, "
+            "Tensor valid_block_num, "
+            "Tensor query_scale, "
+            "Tensor key_scale, "
+            "int tensor_layout, "
+            "int is_causal, "
+            "int qk_quant_gran, "
+            "float sm_scale"
+          ") -> ()");
+    m.def("qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold("
+            "Tensor query, "
+            "Tensor key, "
+            "Tensor value, "
+            "Tensor(a!) output, "
+            "Tensor lut, "
+            "Tensor valid_block_num, "
+            "Tensor pv_threshold, "
+            "Tensor query_scale, "
+            "Tensor key_scale, "
+            "int tensor_layout, "
+            "int is_causal, "
+            "int qk_quant_gran, "
+            "float sm_scale, "
+            "int return_pv_count"
+          ") -> Tensor");
+}
+
+// Registers CUDA implementations
+TORCH_LIBRARY_IMPL(spas_sage_attn_qattn_sm80, CUDA, m) {
+    m.impl("qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf", &qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf);
+    m.impl("qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold", &qk_int8_sv_f16_accum_f16_block_sparse_attn_inst_buf_with_pv_threshold);
 }
