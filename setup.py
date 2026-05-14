067d80cb6b76345c7b8be40e86c7d19a3cf7c4eb
woctordho <woctordho@outlook.com>
2026-05-14 23:50:57 +0800

    Fix std preprocessing in nvcc; don't define _GLIBCXX_USE_CXX11_ABI on Windows

--- setup.py
+++ setup.py
@@ -67,13 +67,15 @@ NVCC_FLAGS_COMMON = [
 if os.name == "nt":
     # https://github.com/pytorch/pytorch/issues/148317
     NVCC_FLAGS_COMMON += [
+        "-Xcompiler=/Zc:preprocessor",
         "-D_WIN32=1",
         "-DUSE_CUDA=1",
     ]
 
-ABI = 1 if torch._C._GLIBCXX_USE_CXX11_ABI else 0
-CXX_FLAGS += [f"-D_GLIBCXX_USE_CXX11_ABI={ABI}"]
-NVCC_FLAGS_COMMON += [f"-D_GLIBCXX_USE_CXX11_ABI={ABI}"]
+if os.name != "nt":
+    ABI = 1 if torch._C._GLIBCXX_USE_CXX11_ABI else 0
+    CXX_FLAGS += [f"-D_GLIBCXX_USE_CXX11_ABI={ABI}"]
+    NVCC_FLAGS_COMMON += [f"-D_GLIBCXX_USE_CXX11_ABI={ABI}"]
 
 if CUDA_HOME is None:
     raise RuntimeError(
